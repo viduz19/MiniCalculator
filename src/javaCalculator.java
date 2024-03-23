e2920652e65e74729cc66c076f5ce88f0a7c3e63
viduz <147256335+viduz19@users.noreply.github.com>
2024-03-22 21:31:34 -0300

    MiniCalculator

--- src/javaCalculator.java
+++ src/javaCalculator.java
@@ -190,6 +190,7 @@ public class javaCalculator extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
